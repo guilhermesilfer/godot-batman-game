e25699c34f49d9ce86303723c4d87b227fde0b4e
guilhermesilfer <guilhermeferreiradev14@gmail.com>
2026-03-25 00:06:20 -0300

    bullet system

--- IMPORT_THIS_FOLDER_INTO_GODOT/media/scripts/player.gd
+++ IMPORT_THIS_FOLDER_INTO_GODOT/media/scripts/player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var Bullet = preload("res://media/scenes/projectile.tscn")
 @onready var _animated_sprite = $AnimatedSprite2D
 
 const SPEED = 180.0
@@ -22,6 +22,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("ui_accept"):
+		var speed: int
+		if $Node2D.position.x > 0:
+			speed = 150
+		if $Node2D.position.x < 0:
+			speed = -150
+		fire(speed)
 
 	move_and_slide()
 
@@ -29,12 +36,25 @@ func _process(_delta):
 	# batman movement logic
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
 		_animated_sprite.play("idle")
+		_animated_sprite.offset = Vector2(0, 0) # resets sprite position
 	elif Input.is_action_pressed("left"):
 		_animated_sprite.flip_h = true
 		_animated_sprite.play("run")
+		_animated_sprite.offset = Vector2(15, 0) # adjust sprite offset for running
+		$Node2D.position.x = -24
 	elif Input.is_action_pressed("right"):
 		_animated_sprite.flip_h = false
 		_animated_sprite.play("run")
+		_animated_sprite.offset = Vector2(-15, 0) # adjust sprite offset for running
+		$Node2D.position.x = 24
 	else:
 		_animated_sprite.play("idle")
+		_animated_sprite.offset = Vector2(0, 0) # resets sprite position
 		
+
+func fire(speed: int):
+	var bullet = Bullet.instantiate()
+	bullet.speed = speed
+	bullet.dir = rotation
+	bullet.pos = $Node2D.global_position
+	get_parent().add_child(bullet)
--- IMPORT_THIS_FOLDER_INTO_GODOT/media/scripts/projectile.gd
+++ IMPORT_THIS_FOLDER_INTO_GODOT/media/scripts/projectile.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+var pos:Vector2
+var dir: float
+var speed: int
+
+func _ready() -> void:
+	global_position = pos
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(speed, 0)
+	move_and_slide()
